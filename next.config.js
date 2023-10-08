5d54d73d175c497cb6904347ccde7496697b25d0
bhandarikishor <kishor@shrigsolutions.com>
2023-10-08 18:15:42 +0545

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  distDir: '_static',
+  images: {
+    unoptimized: true
+  }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig 
+ 
+ 
+ 
+ 
